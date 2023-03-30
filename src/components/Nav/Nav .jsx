b871a4332e672f4985ba02272b1d537ec3ae9901
diego <dacc.dc1224@gmail.comgit congig --global user.name diegogit statuscdgit hepl>
2023-03-29 22:04:30 -0600

    comment

--- src/components/Nav/Nav .jsx
+++ src/components/Nav/Nav .jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SerchBar from " ../components/SearchBar"
 
 export default function Nac(props) {
+  // this is a comment
     return (
         <div>
             <div>
